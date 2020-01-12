30193d88dabecdb22134dfa4650236040dceaf02
muchgangster <jbarringer1999@gmail.com>
2020-01-12 12:38:24 -0800

    merge sort working and insertion sort sucks lmao

--- 1_homework/merge.py
+++ 1_homework/merge.py
@@ -9,15 +9,21 @@ def mergeSort(array):
         half = len(array)//2
         left = array[:half]
         right = array[half:]
-        print(left, right)
+        # print(left, right)
 
         mergeSort(left)
         mergeSort(right)
 
         iter = i = j = 0
         while i < len(left) or j < len(right):
-            print(j, len(right), left[i], right[j])
-            if j == len(right) or left[i] <= right[j]:
+            # print(right, left)
+            if j == len(right):
+                array[iter] = left[i]
+                i += 1
+            elif i == len(left):
+                array[iter] = right[j]
+                j += 1
+            elif left[i] <= right[j]:
                 array[iter] = left[i]
                 i += 1
             else:
@@ -25,6 +31,7 @@ def mergeSort(array):
                 j += 1
             iter += 1
 
+# print(data)
 mergeSort(data)
-print(data)
+# print(data)
 print("--- %s seconds ---" % (time.time() - start_time))
