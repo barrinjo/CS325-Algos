35f5867d51191da26391ec0f3abf032c250a5eee
muchgangster <jbarringer1999@gmail.com>
2020-02-06 18:09:05 -0800

    fixed bug that was messing up my DP algo added empty shopping program

--- 3_homework/knapsack.py
+++ 3_homework/knapsack.py
@@ -7,7 +7,7 @@ def DP(W, wt, val, count):
         for w in range(W + 1):
             if i == 0 or w == 0:
                 memo[i][w] = 0
-            elif wt[i - 1] > W:
+            elif wt[i - 1] > w:
                 memo[i][w] = memo[i - 1][w]
             else:
                 memo[i][w] = max(memo[i - 1][w],
@@ -15,10 +15,10 @@ def DP(W, wt, val, count):
     return memo[count][W]
 
 def recursion(W, wt, val, i):
-    if i == 0 or W == 0:
+    if W == 0 or i == 0:
         return 0
 
-    if W < wt[i - 1]:
+    if W < wt[i - 1] :
         return recursion(W, wt, val, i - 1)
     else:
         return max(recursion(W, wt, val, i - 1),
@@ -33,17 +33,10 @@ for W in range(30, 150, 30):
             val_DP.append(random.randint(1, 100))
         wt_rec = wt_DP.copy()
         val_rec = val_DP.copy()
-
-        print("N={} W = {} || ".format(i, W))
+        print("N={} W={} ".format(i, W), end = " ")
         rec_time = time.time()
-        # print("Rec Max = {}".format(recursion(100, wt_rec, val_rec, i)), end = " ")
-        recursion(W, wt_rec, val_rec, i)
-        print("{}".format("%.5f" % (time.time() - rec_time)))
+        print("Rec Max = {}".format(recursion(W, wt_rec, val_rec, i)), end = " ")
+        print("time = {} ".format("%.5f" % (time.time() - rec_time)), end=" ")
         dp_time = time.time()
-        # print("DP Max = {}".format(DP(100, wt_DP, val_DP, i)), end=" ")
-        DP(W, wt_DP, val_DP, i)
-        print("{}".format("%.5f" % (time.time() - dp_time)))
-
-
-        start_time = time.time()
-        DP
+        print("DP Max = {}".format(DP(W, wt_DP, val_DP, i)), end=" ")
+        print("time = {}".format("%.5f" % (time.time() - dp_time)))
