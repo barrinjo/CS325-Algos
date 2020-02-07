4ef1cfbf3d8ec0dadf1199fc7dab34285117c9a5
muchgangster <jbarringer1999@gmail.com>
2020-02-06 18:37:53 -0800

    added shopping file and am now reading by line

--- 3_homework/shopping.py
+++ 3_homework/shopping.py
@@ -1,3 +1,35 @@
 import random
 import time
 
+# Dynamic programming solution to 0-1 knapsack problem:
+# takes arguments for weight available (W), weight list for items (wt),
+# value list for items (val), and number of items (item)
+def DP(W, wt, val, count):
+    # initialize a memo to store sub-optimizations
+    # memo has an extra column and row of zeros so we don't segfault
+    # and we don't need complex exceptions for border cases
+    memo = [[0 for i in range(W + 1)] for j in range(count + 1)]
+    # nested loop: iterate through entire memo
+    for i in range(count + 1):
+        for w in range(W + 1):
+            # initialize border with 0 value
+            if i == 0 or w == 0:
+                memo[i][w] = 0
+            # if current item can't fit in bad at specified weight,
+            # take optimization of the same weight for the last item
+            elif wt[i - 1] > w:
+                memo[i][w] = memo[i - 1][w]
+            # if the item can fit in the max, take the max between the 
+            # weight option for the previous item optimization or the
+            # value of the current item added with the current optimization
+            # of the current weight minus the weight of the new item
+            else:
+                memo[i][w] = max(memo[i - 1][w],
+                val[i - 1] + memo[i - 1][w - wt[i - 1]])
+    return memo[count][W]
+
+with open("shopping.txt", "r") as file:
+    line = file.readline()
+    while line:
+        print(line, end="")
+        line = file.readline()
