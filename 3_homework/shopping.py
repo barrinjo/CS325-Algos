012b11d86f4561844005059ac4e3d28aa81f5826
muchgangster <jbarringer1999@gmail.com>
2020-02-06 20:19:58 -0800

    shopping algo and file output done. need to comment but then I'm done wew

--- 3_homework/shopping.py
+++ 3_homework/shopping.py
@@ -4,7 +4,7 @@ import time
 # Dynamic programming solution to 0-1 knapsack problem:
 # takes arguments for weight available (W), weight list for items (wt),
 # value list for items (val), and number of items (item)
-def DP(W, wt, val, count):
+def knapsack(W, wt, val, count, subHoldList):
     # initialize a memo to store sub-optimizations
     # memo has an extra column and row of zeros so we don't segfault
     # and we don't need complex exceptions for border cases
@@ -26,10 +26,54 @@ def DP(W, wt, val, count):
             else:
                 memo[i][w] = max(memo[i - 1][w],
                 val[i - 1] + memo[i - 1][w - wt[i - 1]])
+                # if val[i - 1] + memo[i - 1][w - wt[i - 1]] > memo[i - 1][w]:
+                #     holdList[0] = i
+    # for i in range(len(memo)):
+    #     print(memo[i])
+    i = count
+    w = W
+    while memo[i][w] != 0:
+        if memo[i][w] == memo[i-1][w]:
+            i -= 1
+        else:
+            subHoldList.append(i)
+            i -= 1
+            w -= wt[i]
     return memo[count][W]
 
+# small function to find the location of an item in an array
+def lookup(arr, target):
+    count = 0
+    # iterate through until the item is found
+    while count < len(arr) and arr[count] != target:
+        count += 1
+    # return location
+    return count
+
+T = N = f = 0
+out = open("results.txt", "w")
 with open("shopping.txt", "r") as file:
-    line = file.readline()
-    while line:
-        print(line, end="")
-        line = file.readline()
+    T = int(file.readline())
+    for i in range(T):
+        subtotals = []
+        holdList = []
+        wt = []
+        val = []
+        N = int(file.readline())
+        for j in range(N):
+            temp = list(map(int, file.readline().split(" ")))
+            val.append(temp[0])
+            wt.append(temp[1])
+        f = int(file.readline())
+        for k in range(f):
+            subHoldList = []
+            subtotals.append(knapsack(int(file.readline()), wt, val, N, subHoldList))
+            holdList.append(subHoldList)
+        out.write("Test Case {}\n".format(i + 1))
+        out.write("Total Price {}\n".format(sum(subtotals)))
+        out.write("Member Items\n")
+        for j in range(f):
+            out.write("{}: ".format(j+1))
+            for k in range(len(holdList[j]), 0, -1):
+                out.write("{} ".format(holdList[j][k-1]))
+            out.write("\n")
